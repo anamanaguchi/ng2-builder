c94e83f374a69a8be993b7a4d29317be3aa9240c
anamanaguchi <drabikowski@gmail.com>
2017-01-08 21:09:35 +0100

    Unused code removal.

--- src/app/shared/element-helpers.service.ts
+++ src/app/shared/element-helpers.service.ts
@@ -1,35 +1,11 @@
 import { Injectable, Component } from '@angular/core';
 
-import { SectionComponent } from '../section/section.component';
-import { InputComponent } from '../input/input.component';
-
 @Injectable()
 export class ElementHelpersService {
 	
 	private listOfComponents: any[];
 
-	constructor() { 
-		this.listOfComponents = [SectionComponent, InputComponent]
-	}
-
-  	manageElementType(elementType:String){
-		let result = elementType;
-		for(let component of this.listOfComponents){
-			if(component.name === elementType){
-				result = component;
-			}
-		
-		}
-		return result;
-	}
-
-	getElements(configFile){
-		for(let element of configFile.elements){
-			let elementType = element.elementType;
-			element.elementType = this.manageElementType(elementType);
-		}
-		return configFile.elements;
-	}
+	constructor() {}
 
 	getElementClass(attributes){
 		let classArray = [];
