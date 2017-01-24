060a1be56354d4627d8b60e9287fbee7970ee998
michaldrabikowski <drabikowski@gmail.com>
2017-01-24 21:04:21 +0100

    Test github deploy

---
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([2,3],{344:function(n,o){},345:function(n,o){},346:function(n,o){},638:function(n,o,c){c(345),c(344),n.exports=c(346)}},[638]);
+//# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
